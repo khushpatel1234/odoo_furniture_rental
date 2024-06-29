de1e1624a5f72eda2d9627a527a42464eabacc46
khushpatel1234 <110371106+khushpatel1234@users.noreply.github.com>
2024-06-29 17:50:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,8 +26,8 @@ app.post('/create-checkout-session', async (req, res) => {
       quantity: item.quantity,
     })),
     mode: 'payment',
-    success_url: `${req.headers.origin}/`,
-    cancel_url: `${req.headers.origin}/`,
+    success_url: ${req.headers.origin}/,
+    cancel_url: ${req.headers.origin}/,
   });
   res.json({ id: session.id });
 });
@@ -38,9 +38,31 @@ mongoose.connect('mongodb+srv://admin:1234@krishnacluster.2ckhtjo.mongodb.net/?r
   useUnifiedTopology: true,
 });
 
+
+const furnitureSchema = new mongoose.Schema({
+  image: String,
+  title: String,
+  price: Number,
+  stock: String,
+  category: String,
+  delivery: String,
+  days: String,
+  id: Number,
+});
+const Furniture = mongoose.model('Furniture', furnitureSchema);
+
+// API Endpoint to get furniture data
+app.get('/api/furniture', async (req, res) => {
+  try {
+    // Assuming the collection name is 'furniture' and data is nested under an array
+    const data = await Furniture.find({}); // or use a specific query if needed
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 const userSchema = new mongoose.Schema({
   firstname: String,
-  lastname: String,
   email: String,
   password: String,
 });
@@ -48,9 +70,9 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model('OdooUser', userSchema);
 
 app.post('/signup', async (req, res) => {
-  const { firstname,lastname, email, password } = req.body;
+  const { firstname, email, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
-  const newUser = new User({ firstname, lastname, email, password: hashedPassword });
+  const newUser = new User({ firstname, email, password: hashedPassword });
   await newUser.save();
   res.status(201).send({ message: 'User created successfully' });
 });
@@ -65,9 +87,9 @@ app.post('/login', async (req, res) => {
   if (!isPasswordValid) {
     return res.status(400).send({ message: 'Invalid email or password' });
   }
-  const token = jwt.sign({ id: user._id }, 'your_jwt_secret');
+  const token = jwt.sign({ id: user._id }, 'mar123');
   res.send({ message: 'Logged in successfully', token, user });
 });
 
 
-app.listen(4242, () => console.log(`Listening on port 4242!`));
+app.listen(4242, () => console.log(Listening on port 4242!));
